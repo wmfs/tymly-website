3ecac6e345f0751b4fd5f9d0574ea4478c5d51ea
reecebrend <brendy36@gmail.com>
2023-09-11 07:27:33 +0100

    chore: change master to main in manual script [skip ci]

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -4,6 +4,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:wmfs/tymly-website.git master:gh-pages
+git push -f git@github.com:wmfs/tymly-website.git main:gh-pages
 
 cd ../../../
